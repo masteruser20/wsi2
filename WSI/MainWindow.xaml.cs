815dac1ef880c35e0e1647d992d2832c40ee4119
Tomasz Krzykawski <tkrzykawski@us.edu.pl>
2016-04-07 17:20:54 +0200

    .

--- WSI/MainWindow.xaml.cs
+++ WSI/MainWindow.xaml.cs
@@ -1,28 +1,86 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Security.Cryptography;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WSI
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+    
     public partial class MainWindow : Window
     {
+        private bool isAdmin = false;
+
+        public bool Admin
+        {
+            get
+            {
+                return isAdmin;
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void administratorBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+            passwordBox.Visibility = Visibility.Visible;
+
+        }
+
+        private void passwordCancel_Click(object sender, RoutedEventArgs e)
+        {
+            password.Password = "";
+            passwordBox.Visibility = Visibility.Hidden;
+        }
+
+        private void passwordOk_Click(object sender, RoutedEventArgs e)
+        {
+
+            using (SqlConnection con = new SqlConnection(Database.Instance.getConnection().ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+
+                        cmd.Connection = con;
+                        cmd.CommandType = CommandType.Text;
+                        cmd.CommandText = "SELECT password FROM Settings"; 
+                        con.Open();
+
+                        SqlDataAdapter da = new SqlDataAdapter(cmd);
+                        DataTable dt = new DataTable();
+                        da.Fill(dt);
+
+                        DataRow row = dt.Rows[0];
+
+
+                    if (HashAuth.Verify(password.Password, row.Field<string>(0)))
+                    {
+                        isAdmin = true;
+                        dictionariesManag.Visibility = Visibility.Visible;
+                        password.Password = "";
+                        passwordBox.Visibility = Visibility.Hidden;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Błędne hasło.");
+                    }
+
+                    
+
+                }
+            }
+        
+
+            
         }
     }
 }
--- WSI/Properties/Settings.Designer.cs
+++ WSI/Properties/Settings.Designer.cs
@@ -8,22 +8,30 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WSI.Properties
-{
-
-
+namespace WSI.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=tcp:wsi.database.windows.net,1433;Initial Catalog=wsi_database;User I" +
+            "D=wsi@wsi;Password=1QAZ2wsx;Connect Timeout=30;Encrypt=True;TrustServerCertifica" +
+            "te=False")]
+        public string DbString {
+            get {
+                return ((string)(this["DbString"]));
             }
         }
     }
